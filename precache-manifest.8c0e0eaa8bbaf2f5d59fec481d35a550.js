d5adb7836c02939a6c70f796824b7d7a0a5166b3
Alejandro Gemas <alejandro.gemas@gmail.com>
2020-09-22 19:01:10 +0200

    Updates

---
+++ precache-manifest.8c0e0eaa8bbaf2f5d59fec481d35a550.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb24d0076805777d1d07460dd6538212",
+    "url": "/Gif-Explorer-App/index.html"
+  },
+  {
+    "revision": "552e70dde3e846f97ca2",
+    "url": "/Gif-Explorer-App/static/css/main.7472f0c2.chunk.css"
+  },
+  {
+    "revision": "85a4a86f3fe06f17fcc7",
+    "url": "/Gif-Explorer-App/static/js/2.8249eaeb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Gif-Explorer-App/static/js/2.8249eaeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "552e70dde3e846f97ca2",
+    "url": "/Gif-Explorer-App/static/js/main.fe5951c1.chunk.js"
+  },
+  {
+    "revision": "d10ddaac2a6529a19fad",
+    "url": "/Gif-Explorer-App/static/js/runtime-main.ee968b59.js"
+  }
+]);
